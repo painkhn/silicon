da62a5b71c00c1dc16de3ac59f87084c531c44ed
painkhn <1fotice@gmail.com>
2024-05-27 11:21:12 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "index.html",
+    "./src/pages/register.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
