25b5bb44b0ce6b29aab6172de4082e9de0a3fd8f
painkhn <1fotice@gmail.com>
2024-05-28 02:05:18 +0800

    профиль и карточка продукта (пока не адаптировал)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "index.html",
-    "./src/pages/register.html"
+    "./src/pages/register.html",
+    "./src/pages/profile.html",
+    "./src/pages/product.html"
   ],
   theme: {
     extend: {},
