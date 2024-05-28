16ef89713240e6da108fb790a032b19790343010
painkhn <1fotice@gmail.com>
2024-05-28 18:04:28 +0800

    готово

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "index.html",
     "./src/pages/register.html",
     "./src/pages/profile.html",
-    "./src/pages/product.html"
+    "./src/pages/product.html",
+    "./src/pages/search.html",
+    "./src/pages/category.html",
+    "./src/pages/cart.html",
+    "./src/pages/panel.html"
   ],
   theme: {
     extend: {},
